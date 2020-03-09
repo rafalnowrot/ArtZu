8c03c273e044069087ef7ac3c37ed7920c42da85
rafalnowrot <58372360+rafalnowrot@users.noreply.github.com>
2020-03-09 21:05:46 +0100

    Responsibility problem1

--- SklepArtZu/wwwroot/js/site.js
+++ SklepArtZu/wwwroot/js/site.js
@@ -16,3 +16,10 @@ $('a[href^="#"]').on('click', function (event) {
     }
 
 });
+
+
+//$('.banner').css({ 'height': (($(window).height())) +'px'});
+
+//$(window).on('resize', function () {
+//    $('.banner').css({ 'height': (($(window).height())) +'px'});
+//});
