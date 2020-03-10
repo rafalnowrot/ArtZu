90fdd31fdd0f530b175d1960f78fe2191fad20b6
rafalnowrot <58372360+rafalnowrot@users.noreply.github.com>
2020-03-10 20:51:49 +0100

    background-img problem
    still exist...

--- SklepArtZu/wwwroot/js/site.js
+++ SklepArtZu/wwwroot/js/site.js
@@ -12,7 +12,7 @@ $('a[href^="#"]').on('click', function (event) {
         event.preventDefault();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
-        }, 1000);
+        }, 1030);
     }
 
 });
