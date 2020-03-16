80d32c36a3450b862a0fb49db3c49e26749ced7a
rafalnowrot <58372360+rafalnowrot@users.noreply.github.com>
2020-03-16 17:27:15 +0100

    Change logo, add gold color

---
+++ SklepArtZu/Pages/Products/developPageindex.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SklepArtZu.Data;
+using SklepArtZu.Models;
+
+namespace SklepArtZu
+{
+    public class developPageindexModel : PageModel
+    {
+        private readonly SklepArtZu.Data.AppDbContext _context;
+
+        public developPageindexModel(SklepArtZu.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
