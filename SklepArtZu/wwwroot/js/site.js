c64ecc387793eaed09b7c6618e24cca17bea5c71
rafalnowrot <58372360+rafalnowrot@users.noreply.github.com>
2020-03-20 19:16:20 +0100

    Add many staff's

--- SklepArtZu/wwwroot/js/site.js
+++ SklepArtZu/wwwroot/js/site.js
@@ -18,8 +18,17 @@ $('a[href^="#"]').on('click', function (event) {
 });
 
 
-//$('.banner').css({ 'height': (($(window).height())) +'px'});
+var myIndex = 0;
+carousel();
 
-//$(window).on('resize', function () {
-//    $('.banner').css({ 'height': (($(window).height())) +'px'});
-//});
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 9000);
+}
