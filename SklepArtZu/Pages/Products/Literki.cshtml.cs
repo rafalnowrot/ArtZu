541cd3cda3d3d9a3f8d3423baaa55d322db202b5
rafalnowrot <58372360+rafalnowrot@users.noreply.github.com>
2020-03-22 16:46:32 +0100

    Index changes and new view-Literki

---
+++ SklepArtZu/Pages/Products/Literki.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SklepArtZu.Models;
+
+namespace SklepArtZu
+{
+    public class LiterkiModel : PageModel
+    {
+        private readonly SklepArtZu.Data.AppDbContext _context;
+
+        public LiterkiModel(SklepArtZu.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get; set; }
+        
+
+
+        public async Task OnGetAsync()
+        {
+            var product = from products in _context.Products where products.Name == "choineczka" select products;
+
+            Product = await product.ToListAsync();
+        }
+
+
+       
+
+    }
+}
